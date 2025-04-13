ce10f68eb986df6625eb3bc7b3ba3877035b21d6
Burak Tatar <mburaktatar@gmail.com>
2025-04-13 22:11:39 +0300

    Initial commit.

--- src/MethodReferance.java
+++ src/MethodReferance.java
@@ -1,4 +1,23 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MethodReferance {
+
+    public static void main(String[] args) {
+
+        // Ödevde istenilen listeyi ArrayList ile oluşturuyoruz;
+        List<String> names = new ArrayList<>();
+
+        names.add("Ahmet");
+
+        names.add("Ayşe");
+
+        names.add("Mehmet");
+
+        names.add("Zeynep");
+
+        // Method reference ile ise bu listeyi yazdırıyoruz;
+
+        names.forEach(System.out::println);
+    }
 }
